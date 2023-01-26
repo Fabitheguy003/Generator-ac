b11a1448da6d7a973986d871a2a03435c7a0898e
Fabitheguy003 <fabienmukungu@gmail.com>
2023-01-25 19:37:17 -0500

    rewrite script for the password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,31 @@
 // Assignment code here
+function getpassword() {
+
 var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()-()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var passwordLength = 7;
-var password ="";
-
-for (var i = 0; i <= passwordLength; i++)
-
+var passwordLength = 35;
+var password="";
+  
+  for (let i = 0; i<passwordLength; i++) {
+    let randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber,randomNumber +1);
+   
+ }
+return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+ 
+    var password = getpassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
